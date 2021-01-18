70cc8f8c9afbaf9fc17a766249f530780349bd66
Kim Mulligan <kimmariemulligan@gmail.com>
2021-01-17 22:02:21 -0800

    added resolvers and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,42 @@
 const { gql } = require('apollo-server-express');
 
 // create concatenateTypeDefs
-
+const typeDefs = gql `
+type User{
+  _id: ID
+  username: String
+  bookCount: Int
+  savedBooks: [Book]
+}
+type Book{
+  bookId: ID!
+  authors: [String]
+  description: String
+  image: String
+  link: String
+  title: String
+}
+input BookInput{
+  bookId: ID!
+  authors: [String]
+  description: String
+  image: String
+  link: String
+  title: String
+}
+type Auth{
+  token: ID!
+  user: User
+}
+type Query{
+  me: User
+}
+type Mutation{
+  login(email: String!, password: String!):Auth
+  addUser(username: String!, email: String!, password: String!):Auth
+  saveBook(bookData: BookInput!):User
+  removeBook(bookId: ID!):User
+} 
+`
 // export typeDefs
+module.exports = typeDefs;
