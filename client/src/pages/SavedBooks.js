20312fd0a082d4bdc1b05f5031b75d488727020e
Kim Mulligan <kimmariemulligan@gmail.com>
2021-01-26 17:39:17 -0800

    working app

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,43 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useQuery, useMutation } from '@apollo/react-hooks'
+
+
+// import { useQuery } from '@apollo/react-hooks';
+// import { GET_ME } from '../utils/queries';
+
+import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations'
+
+
+// import { useParams } from 'react-router-dom';
 const SavedBooks = () => {
-  const { loading, data } = useQuery(GET_ME)
-  const [removeBook] = useMutation(REMOVE_BOOK)
+  const [userData, setUserData] = useState({});
+  // use this to determine if useEffect() hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+        if (!token) {
+          return false;
+        }
+        const response = await getMe(token);
+        if (!response.ok) {
+          throw new Error('something went wrong!');
+        }
+        const user = await response.json();
+        setUserData(user);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getUserData();
+  }, [userDataLength]);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userData = data?.me || {}
-
+  
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
     if (!token) {
       return false;
     }
-
     try {
-      const { data }= await removeBook({
-        variables: { bookId }
-        
-      });
-
+      const response = await deleteBook(bookId, token);
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+      const updatedUser = await response.json();
+      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
-
   // if data isn't here yet, say so
-  if (loading) {
+  if (!userDataLength) {
     return <h2>LOADING...</h2>;
   }
-
   return (
     <>
       <Jumbotron fluid className='text-light bg-dark'>
@@ -72,5 +93,4 @@ const SavedBooks = () => {
     </>
   );
 };
-
 export default SavedBooks;
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -3,7 +3,7 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import { useMutation } from '@apollo/react-hooks';
 import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -15,7 +15,7 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  const [saveBook] = useMutation(SAVE_BOOK);
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -31,7 +31,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=${searchInput}');
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
