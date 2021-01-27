c0e568415d71f390aed0fceddb224ec5e6b5b5dd
Kim Mulligan <kimmariemulligan@gmail.com>
2021-01-26 18:31:56 -0800

    fiixed middleware issue

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { ApolloServer } = require('apollo-server-express');
-const { AuthMiddleware } = require('.utils/auth')
+const { authMiddleware } = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: AuthMiddleware
+  context: authMiddleware
 });
 server.applyMiddleware({ app });
 
